13420ed8ebc1112231ee015f03dfed3f9bf1a250
HorrySheet11 <marinnathan11@gmail.com>
2025-10-27 20:05:18 +0800

    made working function
    added functions to work with DOM elements and event listeners

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const myLibrary = [];
 
+const bookList = document.getElementById('bookList');
+const button = document.getElementById('addBookBtn');
+
+const authorInput = document.getElementById('authorName');
+const titleInput = document.getElementById('bookName');
+
 function Book(title,author) {
     this.title = title;
     this.author = author;
@@ -9,9 +15,23 @@ function Book(title,author) {
 function addBookToLibrary(title,author) {
     const newBook = new Book(title,author);
     myLibrary.push(newBook);
+    myLibrary.map(book => { 
+        const newBook = document.createElement('div');
+        newBook.classList.add('book');
+        newBook.innerHTML = `<h3>${book.title}</h3><p>${book.author}</p>`;
+        bookList.appendChild(newBook);
+    });
 }
 
-addBookToLibrary("1984","George Orwell");
-addBookToLibrary("To Kill a Mockingbird","Harper Lee");
+// addBookToLibrary("1984","George Orwell");
+// addBookToLibrary("To Kill a Mockingbird","Harper Lee");
 
 console.log(myLibrary);
+
+button.addEventListener('click', () => {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    addBookToLibrary(title,author);
+    titleInput.value = '';
+    authorInput.value = '';
+});
